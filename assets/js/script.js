44416840aa45c9f8d581752ee001fd15263cccd8
alyt89 <alytrodden1@gmail.com>
2022-05-05 13:52:32 +0000

    set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
-console.log("Hello");
+function runGame() {
+
+}
+
+function checkAnswer() {
+    
+}
+
+function calculateCorrectAnswer() {
+    
+}
+
+function incrementScore() {
+    
+}
+
+function incrementWrongAnswer() {
+    
+}
+
+function displayAdditionQuestion() {
+    
+}
+
+function displaySubtractionQuestion() {
+    
+}
+
+function displayMultiplyQuestion() {
+    
+}
