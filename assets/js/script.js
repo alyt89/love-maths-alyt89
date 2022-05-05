8acd8d0e82d837655370ad1a47923a2f8cf95f49
alyt89 <alytrodden1@gmail.com>
2022-05-05 16:45:48 +0000

    add code for division question

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,7 +51,18 @@ else if (gameType === "multiply") {
 else if (gameType === "subtract") {
 
         displaySubtractionQuestion(num1, num2);  
-   
+}
+
+else if (gameType === "division") {
+
+    if (num1 % num2 !== 0) {
+        runGame(gameType);
+    }
+
+    else {
+        displayDivisionQuestion(num1, num2);   
+    }
+    
 }
 else {
     alert(`Unknown game type: ${gameType}`);
@@ -107,6 +118,10 @@ function calculateCorrectAnswer() {
     return [operand1 - operand2, "subtract"];
 }
 
+else if (operator === "/") {
+    return [operand1 / operand2, "division"];
+}
+
  else {
      alert(`Unimplemented operator ${operator}`);
      throw `Unimplemented operator ${operator}. Aborting!`;
@@ -142,5 +157,11 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent  = operand2;
     document.getElementById("operator").textContent  = "x";
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent  = operand2 < operand1 ? operand2 : operand1;
+    document.getElementById("operator").textContent  = "/";
 }
 
